8f291a63bda8f06c409b497b928478ddf5847830
--global <--global>
2026-03-12 18:38:18 -0300

    Add code coverage reports and tests for services
    - Generated HTML coverage reports for services including messaging.ts and persistency.ts.
    - Added a coverage summary for the services in the main index.html.
    - Created detailed coverage reports for messaging.ts and persistency.ts with full coverage statistics.
    - Implemented sorting functionality in the coverage summary using sorter.js.
    - Added a search filter for the coverage report.
    - Included a new PNG sprite for sorting indicators.
    - Created a new lcov.info file to track code coverage metrics.
    - Added unit tests for the Order class to ensure proper functionality during checkout.

---
+++ src/classes/order.spec.ts
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Item } from './interfaces/cart-item';
+import { CustomerProtocol } from './interfaces/customer-protocol';
+import { MessagingProtocol } from './interfaces/messaging-protocol';
+import { PersistencyProtocol } from './interfaces/persistency-protocol';
+import { ShoppingCartProtocol } from './interfaces/shopping-cart-protocol';
+import { Order } from './order';
+
+class ShoppingCartMock implements ShoppingCartProtocol {
+  get items(): Readonly<Item[]> {
+    return [];
+  }
+  addItem(item: Item): void {}
+  removeItem(index: number): void {}
+  total(): number {
+    return 0;
+  }
+  totalWithDiscount(): number {
+    return 0;
+  }
+  isEmpty(): boolean {
+    return false;
+  }
+  clear(): void {}
+}
+
+class MessagingMock implements MessagingProtocol {
+  sendMessage() {}
+}
+
+class PersistencyMock implements PersistencyProtocol {
+  saveOrder() {}
+}
+
+class CustomerMock implements CustomerProtocol {
+  getName() {
+    return 'Customer Mock';
+  }
+
+  getIDN() {
+    return '12345678900';
+  }
+}
+
+const createSut = () => {
+  const shoppingCartMock = new ShoppingCartMock();
+  const messagingMock = new MessagingMock();
+  const persistencyMock = new PersistencyMock();
+  const customerMock = new CustomerMock();
+
+  const sut = new Order(
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+    customerMock,
+  );
+
+  return {
+    sut,
+    shoppingCartMock,
+    messagingMock,
+    persistencyMock,
+    customerMock,
+  };
+};
+
+describe('Order', () => {
+  it('shloud not checkout if the cart is empty', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest
+      .spyOn(shoppingCartMock, 'isEmpty')
+      .mockReturnValue(true);
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe('open');
+  });
+
+  it('shloud checkout if the cart is not empty', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest
+      .spyOn(shoppingCartMock, 'isEmpty')
+      .mockReturnValue(false);
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+    expect(sut.orderStatus).toBe('closed');
+  });
+
+  it('shloud send a message if the checkout is successful', () => {
+    const { sut, messagingMock } = createSut();
+    const messagingMockSpy = jest.spyOn(messagingMock, 'sendMessage');
+    sut.checkout();
+    expect(messagingMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('shloud save the order if the checkout is successful', () => {
+    const { sut, persistencyMock } = createSut();
+    const persistencyMockSpy = jest.spyOn(persistencyMock, 'saveOrder');
+    sut.checkout();
+    expect(persistencyMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('shloud clear the cart if the checkout is successful', () => {
+    const { sut, shoppingCartMock } = createSut();
+    const shoppingCartMockSpy = jest.spyOn(shoppingCartMock, 'clear');
+    sut.checkout();
+    expect(shoppingCartMockSpy).toHaveBeenCalledTimes(1);
+  });
+});
