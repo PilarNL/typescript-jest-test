bb75ef129c73e5c6b4bee58ac4d07e0c568e5930
--global <--global>
2026-03-12 11:47:47 -0300

    test: add unit tests for Messaging service

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,36 @@
+import { Messaging } from './messaging';
+
+const createSut = () => {
+  return new Messaging();
+};
+
+describe('Messaging', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return undefined', () => {
+    //Ssystem under test
+    const sut = createSut();
+    expect(sut.sendMessage('Test message')).toBeUndefined();
+  });
+
+  it('should call console.log with the correct message', () => {
+    //Ssystem under test
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('Test message');
+    expect(consoleSpy).toHaveBeenCalledWith(
+      'Mensagem enviada:',
+      'Test message',
+    );
+  });
+
+  it('should call console.log once', () => {
+    //Ssystem under test
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('Test message');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+});
